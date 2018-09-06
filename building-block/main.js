4884707958f55a3b3343db9a84701ed59cbacb0f
Vaibhav2812 <patil.vaibhav2812@gmail.com>
2018-09-06 06:55:39 +0530

    step-2-building block(added switch and ternary operator)

--- building-block/main.js
+++ building-block/main.js
@@ -1,22 +1,58 @@
-var select = document.querySelector('select');
-var para = document.querySelector('p');
-if(select) {
-    select.addEventListener('change', setWeather);
+var select = document.querySelector('#weather');
+var para = document.querySelector('#weather-info');
+var theme = document.querySelector('#theme');
+var html = document.querySelector('html');
+document.body.style.padding = '20px';
+if (select) {
+  select.addEventListener('change', getWeather);
 }
 
 
 function setWeather() {
-    var choice = select.value;
-    if (choice === 'sunny') {
+  var choice = select.value;
+  if (choice === 'sunny') {
+    para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
+  } else if (choice === 'rainy') {
+    para.textContent = 'Rain is falling outside; take a rain coat and a brolly, and don\'t stay out for too long.';
+  } else if (choice === 'snowing') {
+    para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
+  } else if (choice === 'overcast') {
+    para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
+  } else {
+    para.textContent = '';
+  }
+}
+
+
+function getWeather() {
+  var choice = select.value;
+  switch (choice) {
+    case 'sunny':
       para.textContent = 'It is nice and sunny outside today. Wear shorts! Go to the beach, or the park, and get an ice cream.';
-    } else if (choice === 'rainy') {
+      break;
+
+    case 'rainy':
       para.textContent = 'Rain is falling outside; take a rain coat and a brolly, and don\'t stay out for too long.';
-    } else if (choice === 'snowing') {
+      break;
+
+    case 'snowing':
       para.textContent = 'The snow is coming down — it is freezing! Best to stay in with a cup of hot chocolate, or go build a snowman.';
-    } else if (choice === 'overcast') {
+      break;
+
+    case 'overcast':
       para.textContent = 'It isn\'t raining, but the sky is grey and gloomy; it could turn any minute, so take a rain coat just in case.';
-    } else {
-      para.textContent = '';
-    }
+      break;
+
+    default:
+      para.textContent = ''
   }
 
+  function update(bgColor,txtColor) {
+    html.style.backgroundColor = bgColor;
+    html.style.color = txtColor
+  }
+
+  themeSelect.onchange = function () {
+    theme.value === 'black' ? update('black','white'): update('white','black');
+  }
+}
