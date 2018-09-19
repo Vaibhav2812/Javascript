b5f98fbe08f1a343165caecf1e296796ba247f85
vaibhav.patil <vaibhav.patil@greytip.com>
2018-09-19 17:18:40 +0530

    Step2-building-block(added year in calendar).

--- building-block/calendar/main.js
+++ building-block/calendar/main.js
@@ -1,16 +1,22 @@
-var month = document.querySelector('#month');
+var monthList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 var list = document.querySelector('ul');
+var years = document.querySelector('#year');
+var months = document.querySelector('#month');
 var selectedMonth = document.querySelector('h1');
+var month;
+var days;
+var year;
+var startYear = 1900;
+months.onchange = function () {
+    month = months.value;
+    days = getDays(month, year);
+    createCaledar(days, month);
+}
 
-month.onchange = function () {
-    var choice = month.value;
-    var days = 31;
-    if (choice === 'february') {
-        days = 28;
-    } else if (choice === 'april' || choice === 'june' || choice === 'september' || choice === 'november') {
-        days = 30;
-    }
-    createCaledar(days, choice);
+years.onchange = function () {
+    year = years.value;
+    days = getDays(month, year);
+    createCaledar(days, month);
 }
 
 function createCaledar(days, choice) {
@@ -23,4 +29,38 @@ function createCaledar(days, choice) {
     }
 }
 
+function getDays(m, y) {
+    days = 31;
+    if (m === 'Feb') {
+        if (y % 4 === 0) {
+            days = 29
+        } else {
+            days = 28;
+        }
+    } else if (m === 'Apr' || m === 'Jun' || m === 'Sep' || m === 'Nov') {
+        days = 30;
+    }
+    return days;
+}
+const createYear = (sy) => {
+    const currentYear = new Date().getFullYear();
+    years.innerHTML = '';
+    while (sy <= currentYear) {
+        var options = document.createElement('option');
+        options.textContent = sy;
+        years.appendChild(options);
+        sy++;
+    }
+}
+
+const createMonth = (m) => {
+    months.innerHTML = '';
+    for (var i = 0; i < m.length; i++) {
+        var options = document.createElement('option');
+        options.textContent = m[i];
+        months.appendChild(options);
+    }
+}
 createCaledar(31, 'january');
+createYear(startYear);
+createMonth(monthList);
