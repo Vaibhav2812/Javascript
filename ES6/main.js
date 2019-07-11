96ee2d450c45130283ea540d8701894575bd7466
vaibhav_patil <vaibhav.laxmanpatil@narayanahealth.org>
2019-07-11 09:55:14 +0530

    initial commit for destructuring

--- ES6/main.js
+++ ES6/main.js
@@ -32,3 +32,45 @@ var o2 = {};
 ([o2.a, o2.b, o2.c] = a1);
 console.log(o2); //{a:1, b:2, c: 3}
 
+//tagged template literals
+
+const name= "sean";
+const wishes = "Good morning";
+let message = tag`Hi hello there ${name} wish you ${wishes}`;
+
+function tag(literals, ...substitutions) {
+     let result;
+    substitutions.forEach((value, index) => {
+        result += literals[index] + substitutions[index];
+    });
+    return result;
+}
+console.log(message);
+
+
+//repeated assignment
+ var {a: {x:X}, a}= {a: {x:1}};
+ console.log(X); //1
+ console.log(a); //{x:1}
+
+ ({a:X, a:Y , a: [Z]} = {a: [1]});
+ X.push(2);
+ Y[0] = 10;
+ console.log(X);
+ console.log(Y);
+ console.log(Z);
+
+ //Remember: the purpose of destructuring is not just less typing,
+//   but more declarative readability.
+
+//assignment expression
+var o2 = { a: 1, b:2, c: 3,d: 4};
+var o3 = {a,b,c} = o2; //1,2,3
+console.log(o3); //{ a: 1, b:2, c: 3,d: 4}
+var a2 = [1,2,3], a,b,c,a3;
+ a3 = [a,b,c] = a2;
+ console.log(a3);
+
+ //chaining destructring assignmet
+({a} = {b,c} = o2);
+console.log(a);
